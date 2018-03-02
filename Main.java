586e94751dfdd431d940763c64dbdae11fa3b259
Heros Noriega <hnoriegajuarez@gmail.com>
2018-03-01 23:33:58 -0600

    Added methods in Main

--- Main.java
+++ Main.java
@@ -1,8 +1,5 @@
 package com.herosnoriega.amazonviewer;
-
 import java.util.Scanner;
-
-import com.herosnoriega.amazonviewer.model.Movie;
 
 public class Main {
 
@@ -13,48 +10,94 @@ public class Main {
 				
 	}
 	
-	public static void showMenu() {
-		
-		int exit = 0;
-		
+	public static void showMenu() {	
+
+		int exit = 1;	
 		do {
 			
+			Scanner scanner = new Scanner(System.in);
+			
 			System.out.println("BINVENIDOS AMAZON VIEWER");
-			System.err.println("");
+			System.out.println("");
 			System.out.println("Selecciona el numero de la opcion deseada");
 			System.out.println("1. Movies");
 			System.out.println("2. Series");
-			System.out.println("3. Books");
-			System.out.println("4. Magazines");
+			System.out.println("3: Chapters");
+			System.out.println("4. Books");
+			System.out.println("5. Magazines");
 			System.out.println("0. Exit");
+			System.out.println("");
 			
 			//reads response from user
-			Scanner sc = new Scanner(System.in);
-			int response = Integer.valueOf(sc.nextLine());
-			switch (response) {
+			
+			int response = Integer.valueOf(scanner.nextLine());
+			
+			switch(response) {
 			case 0:
-				System.out.println("Saliendo del Sistema");
-				exit = 1;
+				exit = 0;
+				System.out.println("");
+				System.out.println("Saliendo del Sistema!");
+				System.out.println("");
 				break;
+			
 			case 1:
-				System.out.println("Visto Movies!");
+				showMovies();
 				break;
+			
 			case 2:
-				System.out.println("Visto Series!");
+				showSeries();
 				break;
+			
 			case 3:
-				System.out.println("Visto Books!");
+				showChapters();
 				break;
+				
 			case 4:
-				System.out.println("Visto Magazine!");
+				showBooks();
 				break;
+				
+			case 5:
+				showMagazines();
+				break;
+				
 			default:
-				System.out.println("Seleccion no valida.");
+				System.out.println("");
+				System.out.println(":: Selecciona una opcion! ::");
+				System.out.println("");
 				break;
 			}
 			
 		}while(exit != 0);
-
 	}
 
+	public static void showMovies() {
+		System.out.println();
+		System.out.println(":: MOVIES ::");
+		System.out.println();
+	}
+	
+	public static void showSeries() {
+		System.out.println();
+		System.out.println(":: SERIES ::");
+		System.out.println();
+	}
+	
+	public static void showChapters() {
+		System.out.println();
+		System.out.println(":: CHAPTERS ::");
+		System.out.println();
+	}
+	
+	public static void showBooks() {
+		System.out.println();
+		System.out.println(":: BOOKS ::");
+		System.out.println();
+	}
+	
+	public static void showMagazines() {
+		System.out.println();
+		System.out.println(":: MAGAZINES ::");
+		System.out.println();
+	}
+	
 }
